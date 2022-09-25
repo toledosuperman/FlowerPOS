1c0f68ed9a728ac155841385c508cf91645d96db
toledosuperman <hf7616@wayne.edu>
2022-09-24 20:24:41 -0400

    new order OrderForm

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,93 +1,167 @@
-import Form from 'react-bootstrap/Form';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
 
+function OrderForm ()  {
+return (
+  <form>
+    <h1>Customer Info</h1>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Customer Name</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Customer Address</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Customer City</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">State</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Zip</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Phone</label>
+</div>
+<h1>Recipient Info</h1>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment"> Name</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment"> Address</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment"> City</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">State</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Zip</label>
+</div>
+<div class="form-floating">
+  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
+  <label for="comment">Phone</label>
+</div>
+</form>
+    // <Form>
+    //   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="10">Order Number</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-  const OrderForm = () => {
-    const { user, logout } = UserAuth();
-    const navigate = useNavigate();
-  return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Order Number</Form.Label>
-        <Form.Control type="Order Number"/>
-      </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer Name</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer Name</Form.Label>
-        <Form.Control type="Customer Name"/>
-      </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer Address</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer Address</Form.Label>
-        <Form.Control type="Customer Address"/>
-      </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer City</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer City</Form.Label>
-        <Form.Control type="Customer City"/>
-      </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer State</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer State</Form.Label>
-        <Form.Control type="Customer State"/>
-      </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer Zip</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer Zip</Form.Label>
-        <Form.Control type="Customer Zip"/>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Customer Phone</Form.Label>
-        <Form.Control type="Customer Phone"/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Recipient Name</Form.Label>
-          <Form.Control type="Recipient Name"/>
-        </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //     <Form.Label column sm="2">Customer Phone</Form.Label>
+    //     <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //   </Form.Group>
+    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2" >Recipient Name</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Recipient Address</Form.Label>
-          <Form.Control type="recipient Address"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Recipient Address</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Recipient City</Form.Label>
-          <Form.Control type="Recipient City"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Recipient City</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Recipient State</Form.Label>
-          <Form.Control type="Recipient State"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Recipient State</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Recipient Zip</Form.Label>
-          <Form.Control type="Recipient Zip"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Recipient Zip</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Recipient Phone Number</Form.Label>
-          <Form.Control type="Recipient Phone Number"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Recipient Phone Number</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Delivery Date</Form.Label>
-          <Form.Control type="Delivery Date"/>
-        </Form.Group>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Delivery Date</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
   
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Product</Form.Label>
-          <Form.Control type="Product"/>
-        </Form.Group>
-    </Form>
+    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
+    //       <Form.Label column sm="2">Product</Form.Label>
+    //       <Col sm="10">
+    //       <Form.Control type="text" placeholder="Normal text" />
+    //       </Col>
+    //     </Form.Group>
+    // </Form>
 
     
   );
   
-  
-}
+};
 
 export default OrderForm;
