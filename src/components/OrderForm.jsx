3cbca35d28acf25b57c29e2004526c95433dda92
toledosuperman <hf7616@wayne.edu>
2022-09-26 16:06:22 -0400

    submit button

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,3 +1,9 @@
+import { initializeApp, db } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+
+
+
 
 function OrderForm ()  {
 return (
@@ -71,114 +77,11 @@ return (
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> Delivery Date</label>
 </div>
+<input type="submit" value="Submit" />
 </div>
 </form>
-    // <Form>
-    //   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="10">Order Number</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer Name</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer Address</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer City</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer State</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer Zip</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //     <Form.Label column sm="2">Customer Phone</Form.Label>
-    //     <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //   </Form.Group>
-    //   <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2" >Recipient Name</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Recipient Address</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Recipient City</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Recipient State</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Recipient Zip</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Recipient Phone Number</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Delivery Date</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-  
-    //     <Form.Group className="mb-3"  controlId="exampleForm.ControlInput1">
-    //       <Form.Label column sm="2">Product</Form.Label>
-    //       <Col sm="10">
-    //       <Form.Control type="text" placeholder="Normal text" />
-    //       </Col>
-    //     </Form.Group>
-    // </Form>
-
+   
+    
     
   );
   
