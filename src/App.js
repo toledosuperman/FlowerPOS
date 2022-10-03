2bceca05a41421efd724b61547ee9aeb2458bfc4
toledosuperman <hf7616@wayne.edu>
2022-10-03 16:04:59 -0400

    added pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import OrderForm from './components/OrderForm';
 import Signin from './components/Signin';
 import ViewOrders from './components/ViewOrders';
+import ViewProducts from './components/ViewProducts';
 import Signup from './components/Signup';
 import Account from './components/Account';
 import { Route, Routes } from 'react-router-dom';
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path='/order' element={<OrderForm />} />
           <Route path='/vieworder' element={<ViewOrders />} />
+          <Route path='/viewproduct' element={<ViewProducts />} />
           <Route path='/' element={<Signin />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/account' element={
---
+++ src/components/ViewProducts.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
+const ViewProducts= () => {
+    
+return (
+    <div className='max-w-[600px] mx-auto my-16 p-4'>
+      <h1 className='text-2xl font-bold py-4'>View Orders</h1>
+      
+
+      
+        
+    </div>
+  );
+};
+
+export default ViewProducts;
