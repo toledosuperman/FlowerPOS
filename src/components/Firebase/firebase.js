92b3ddb338119ff21e13c072a75e31e2fe021b68
toledosuperman <hf7616@wayne.edu>
2022-10-17 11:17:56 -0400

    orderform update

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,7 @@
-import { getDatabase } from "firebase/database";
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore,
-  query,
-  orderBy,
-  onSnapshot,
-  collection,
-  getDoc, 
-  getDocs, 
-  addDoc,
-  updateDoc,
-  doc, 
-  serverTimestamp, 
-  arrayUnion } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcc1jnbceSvBcBmDuN2nnMzMqAw3euNqE",
@@ -25,8 +14,9 @@ const firebaseConfig = {
   measurementId: "G-D9XSKRBLXV"
   };
   const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
- export const database = getDatabase();
+ 
+
   export const auth = getAuth(app);
-  export {db,app}
+  export const db = getFirestore(app)
+  export {app}
   
--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,45 +1,55 @@
-import { addDoc, collection, doc, setdoc } from "firebase/firestore"; 
-import { getDatabase, ref, set } from "firebase/database";
+// import { addDoc, collection } from "firebase/firestore"; 
+import {db} from '../components/Firebase/firebase'
+import { collection, doc, addDoc } from "firebase/firestore"; 
 import { useForm } from "react-hook-form";
 import React, { useState, useEffect } from "react";
-import { UserAuth } from '../context/AuthContext';
-import {  useNavigate } from 'react-router-dom';
 
 
-
-
-function OrderForm({ values, submit }) {
-  // initialize react-hook-form
-  const db = getDatabase();
-  const { register, reset } = useForm();
-  const { user, logout } = UserAuth();
-  const navigate = useNavigate();
+const OrderForm = () => {
+ 
   const [customername  , Setcustomername] = useState("");
   const [customeraddress , Setcustomeraddress] = useState("");
   const [customercity , Setcustomercity] = useState("");
+  const [customerstate  , Setcustomerstate] = useState("");
+  const [customerzip , Setcustomerzip] = useState("");
+  const [customerphone , Setcustomerphone] = useState("");
+  const [customeremail  , Setcustomeremail] = useState("");
+  const [recipientname  , Setrecipientname] = useState("");
+  const [recipientaddress , Setrecipientaddress] = useState("");
+  const [recipientcity , Setrecipientcity] = useState("");
+  const [recipientstate , Setrecipientstate] = useState("");
+  const [recipientzip , Setrecipientzip] = useState("");
+  const [recipientphone , Setrecipientphone] = useState("");
+  const [product, Setproduct] = useState("");
+  const [deliverydate , Setdeliverydate] = useState("");
   const handleSubmit = async(e) => {
         e.preventDefault();
         try {
           const docRef = await addDoc(collection(db, "Orders"), {
             CustomerName: customername,
             CustomerAddress: customeraddress,
-            CustomerCity: customercity
+            CustomerCity: customercity,
+            CustomerState: customerstate,
+            CustomerZip: customerzip,
+            CustomerPhone: customerphone,
+            CustomerEmail: customeremail,
+            RecipientName: recipientname,
+            RecipientAddress: recipientaddress,
+            RecipientCity: recipientcity,
+            RecipientState: recipientstate,
+            RecipientZip: recipientzip,
+            RecipientPhone: recipientphone,
+            Product: product,
+            DeliveryDate: deliverydate
           });
           console.log("Document written with ID: ", docRef.id);
         } catch (e) {
           console.error("Error adding document: ", e);
         }
-      }
-  useEffect(() => {
-    reset(values);
-  }, [values]);
-
-  // call container submit handler to save new/updated values
-  const onSubmit = (submittedData) => {
-    submit(submittedData);
-  };
+      
 
 
+  
 
 
 return (
@@ -63,72 +73,67 @@ return (
   <label for="comment">Customer City</label>
   <input type="text" onChange={(e)=>{Setcustomercity(e.target.value)}} />
 </div>
-<div class="form-floating">
+ <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">State</label>
-  <input type="text" {...register("CustomerState")} />
+  <input type="text" onChange={(e)=>{Setcustomerstate(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">Zip</label>
-  <input type="text" {...register("CustomerZip")} />
+  <input type="text" onChange={(e)=>{Setcustomerzip(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">Phone</label>
-  <input type="text" {...register("CustomerPhone")} />
+  <input type="text" onChange={(e)=>{Setcustomerphone(e.target.value)}}/>
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">Email</label>
-  <input type="text" {...register("CustomerEmail")} />
+  <input type="text" onChange={(e)=>{Setcustomeremail(e.target.value)}} />
 </div>
 <h1>Recipient Info</h1>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> Name</label>
-  <input type="text" {...register("RecipientName")} />
+  <input type="text" onChange={(e)=>{Setrecipientname(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> Address</label>
-  <input type="text" {...register("RecipientAddress")} />
+  <input type="text"  onChange={(e)=>{Setrecipientaddress(e.target.value)}}/>
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> City</label>
-  <input type="text" {...register("RecipientCity")} />
+  <input type="text"  onChange={(e)=>{Setrecipientcity(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">State</label>
-  <input type="text" {...register("RecipientState")} />
+  <input type="text"  onChange={(e)=>{Setrecipientstate(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">Zip</label>
-  <input type="text" {...register("RecipientZip")} />
+  <input type="text" onChange={(e)=>{Setrecipientzip(e.target.value)}} />
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment">Phone</label>
-  <input type="text" {...register("RecipientPhone")} />
-</div>
-<div class="form-floating">
-  <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
-  <label for="comment">Email</label>
-  <input type="text" {...register("RecipientEmail")} />
+  <input type="text"  onChange={(e)=>{Setrecipientphone(e.target.value)}}/>
 </div>
 <h1>Product Info</h1>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> Product</label>
-  <input type="text" {...register("Product")} />
+  <input type="text"  onChange={(e)=>{Setproduct(e.target.value)}}/>
 </div>
 <div class="form-floating">
   <textarea class="form-control" id="comment" name="text" placeholder="Comment goes here"></textarea>
   <label for="comment"> Delivery Date</label>
-  <input type="text" {...register("DeliveryDate")} />
+  <input type="text"  onChange={(e)=>{Setdeliverydate(e.target.value)}} /> 
 </div>
 <button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
           Submit
@@ -136,10 +141,11 @@ return (
 </div>
 </form>
    
-    
+ 
     
   );
   
-};
+
+}}
 
 export default OrderForm;
