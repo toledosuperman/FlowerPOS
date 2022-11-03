3cf3fb310fb4ba38fe3fe30ae8a9d76cbd012c21
toledosuperman <hf7616@wayne.edu>
2022-11-03 18:18:27 -0400

    adding to recipe page

--- src/components/CartScreen.js
+++ src/components/CartScreen.js
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import { CartContainerStyle, PageHeading } from '../components/styles/CartScreen'
 import { listCartItems } from '../components/cartActions'
 import CartItem from './CartItem'
 import { useDispatch, useSelector } from 'react-redux'
 import Navbar from './Nav/navbar';
+import {db} from '../firebase'
+import {onSnapshot, collection, addDoc, Timestamp,} from 'firebase/firestore';
 
 const CartScreen = () => {
+  useEffect(() =>
+      onSnapshot(collection(db, "Products"), (snapshot) => console.log(snapshot.docs)
+    ));
   const dispatch = useDispatch()
-
+  const[Name, setName]= useState('');
   const cartItemsList = useSelector((state) => state.cartItemsList)
 
   const { loading, error, cartItems } = cartItemsList
