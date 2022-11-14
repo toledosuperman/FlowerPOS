dfec3d735c547fa2a09e14f0c9e51eda005b7ef0
sayemk23 <go2218@wayne.edu>
2022-11-14 17:06:30 -0500

    navbar edit and home page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,29 +1,72 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, {  useState } from "react";
+import {NavLink, useNavigate} from 'react-router-dom';
 import './navbar.css';
 import logo from '../assets/logo.png'
-const navbar = props =>(
-<header className= "navbar">
-<a className="navbar-brand" href="/">
-      <div className="logo-image">
-            <img src= {logo} className="img-fluid"alt=""/>
-      </div>
-</a> 
-<div className="navbarLogo">
-<h1 className='text-left text-3xl font-bold'>
-        Welcome to Flower POS!
-      </h1>
-</div>
-<nav className ="navbarList">
-<ul id="navlinks">
-<li><NavLink to="/account">Home</NavLink> </li>
-<li><NavLink to="/order">New Order</NavLink> </li>
-<li><NavLink to="/vieworders">View Orders</NavLink> </li>
-<li><NavLink to="/viewproduct">Products</NavLink> </li>
-<li><NavLink to="/createrecipe">Create Recipes</NavLink> </li>
-{/* <li><NavLink to="/customers">Customers</NavLink> </li> */}
-</ul>
-</nav>
-</header>
-);
-export default navbar;
+import { Navbar, Nav, NavDropdown, Form, Button, ButtonGroup } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import NoLoggedInView from '../components/NoLoggedInView';
+import { UserAuth} from '../context/AuthContext';
+
+
+const NavBar = ({ }) => {
+ 
+      const { user, logout } = UserAuth();
+      const navigate = useNavigate();
+      const handleLogout = async () => {
+            try {
+              await logout();
+              navigate('/account');
+              console.log('You are logged out')
+            } catch (e) {
+              console.log(e.message);
+            }
+          };
+  
+      const authButton = () => {
+          if (user !== null) {
+              
+              return <NavLink onClick={handleLogout}>Logout</NavLink>
+          }
+      }
+  
+      return (
+            <><header className="navbar">
+                  <a className="navbar-brand" href="/">
+                        <div className="logo-image">
+                              <img src={logo} className="img-fluid" alt="" />
+                        </div>
+                  </a>
+                  <div className="navbarLogo">
+                        <h1 className='text-left text-3xl font-bold'>
+                              Welcome to Flower POS!
+                        </h1>
+                  </div>
+                  <nav className="navbarList">
+                        <ul id="navlinks">
+                              <li><NavLink to="/account">Home</NavLink> </li>
+                              <li><NavLink to="/order">New Order</NavLink> </li>
+                              <li><NavLink to="/vieworders">View Orders</NavLink> </li>
+                              <li><NavLink to="/viewproduct">Products</NavLink> </li>
+                              <li><Form inline>
+                        {authButton()}
+                  </Form>
+                  </li>
+                              {/* <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li> <NavLink class="dropdown-item" onClick={authButton()}>Logout</NavLink></li>
+                    </ul>
+                              </li> */}
+                        </ul> 
+                  </nav>
+            </header></>
+          
+      )
+  }
+  
+  function mapStateToProps(state) {
+      return { user: state.user }
+    }
+  
+  export default connect(mapStateToProps, {} )(NavBar)
+
