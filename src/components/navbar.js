d0d3ffe7d98aa253cd7382b2932d6d5def68cc60
toledosuperman <49626128+toledosuperman@users.noreply.github.com>
2022-11-15 17:02:12 -0500

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,20 @@
-import React, {  useState } from "react";
-import {NavLink, useNavigate} from 'react-router-dom';
+import React from "react";
+import {NavLink} from 'react-router-dom';
 import './navbar.css';
 import logo from '../assets/logo.png'
-import { Navbar, Nav, NavDropdown, Form, Button, ButtonGroup } from 'react-bootstrap'
+import {  Form, } from 'react-bootstrap'
 import { connect } from 'react-redux'
-import NoLoggedInView from '../components/NoLoggedInView';
 import { UserAuth} from '../context/AuthContext';
 
 
-const NavBar = ({ }) => {
+const NavBar = (a={ }) => {
  
       const { user, logout } = UserAuth();
-      const navigate = useNavigate();
+      
       const handleLogout = async () => {
             try {
               await logout();
-              navigate('/account');
+              
               console.log('You are logged out')
             } catch (e) {
               console.log(e.message);
@@ -47,16 +46,11 @@ const NavBar = ({ }) => {
                               <li><NavLink to="/order">New Order</NavLink> </li>
                               <li><NavLink to="/vieworders">View Orders</NavLink> </li>
                               <li><NavLink to="/viewproduct">Products</NavLink> </li>
-                              <li><Form inline>
+                              <li><NavLink to="/createrecipe">Create Recipes</NavLink> </li>
+                              <li><Form inline= "true">
                         {authButton()}
                   </Form>
                   </li>
-                              {/* <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li> <NavLink class="dropdown-item" onClick={authButton()}>Logout</NavLink></li>
-                    </ul>
-                              </li> */}
                         </ul> 
                   </nav>
             </header></>
