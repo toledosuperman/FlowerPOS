c55c39fa2d4e44e6bb0d0a96c08c56420608bc28
toledosuperman <49626128+toledosuperman@users.noreply.github.com>
2022-11-16 16:04:13 -0500

    order edit

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -5,15 +5,14 @@ import {db} from '../firebase'
 import {collection, addDoc, Timestamp} from 'firebase/firestore';
 import Navbar from './navbar';
 import NoLoggedInView from '../components/NoLoggedInView';
-import { Spinner } from 'react-bootstrap';
+import { Spinner, Button } from 'react-bootstrap';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 function OrderForm({  onClose,open}) {
         const [isLoading] = useState(false);
         const { user } = UserAuth();
-        // const [value, onChange] = useState(new Date());
-  // initialize react-hook-form
+        
   
   const[CustomerName, setCustomerName]= useState('');
   const[ CustomerCity,setCustomerCity ]= useState('');
@@ -89,99 +88,82 @@ return (<>
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here" onChange={(e) => setCustomerCity(e.target.value.toUpperCase())} 
           value={CustomerCity}></textarea>
   <label htmlFor="comment">Customer City</label>
-  {/* <input type="text" onChange={(e) => setCustomerCity(e.target.value.toUpperCase())} 
-          value={CustomerCity} /> */}
+ 
 </div>
  <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setCustomerState(e.target.value.toUpperCase())} 
           value={CustomerState}></textarea>
   <label htmlFor="comment">State</label>
-  {/* <input type="text" onChange={(e) => setCustomerState(e.target.value.toUpperCase())} 
-          value={CustomerState} /> */}
+  
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setCustomerZip(e.target.value.toUpperCase())} 
           value={CustomerZip}></textarea>
   <label htmlFor="comment">Zip</label>
-  {/* <input type="text" onChange={(e) => setCustomerZip(e.target.value.toUpperCase())} 
-          value={CustomerZip} /> */}
+  
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setCustomerPhone(e.target.value.toUpperCase())} 
           value={CustomerPhone} ></textarea>
   <label htmlFor="comment">Phone</label>
-  {/* <input type="text" onChange={(e) => setCustomerPhone(e.target.value.toUpperCase())} 
-          value={CustomerPhone} /> */}
+ 
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setCustomerEmail(e.target.value.toUpperCase())} 
           value={CustomerEmail} ></textarea>
   <label htmlFor="comment">Email</label>
-  {/* <input type="text" onChange={(e) => setCustomerEmail(e.target.value.toUpperCase())} 
-          value={CustomerEmail} /> */}
+ 
 </div>
 <h1>Recipient Info</h1>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setRecipientName(e.target.value.toUpperCase())} 
           value={RecipientName}></textarea>
   <label htmlFor="comment"> Name</label>
-  {/* <input type="text" onChange={(e) => setRecipientName(e.target.value.toUpperCase())} 
-          value={RecipientName} /> */}
+ 
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here" onChange={(e) => setRecipientAddress(e.target.value.toUpperCase())} 
           value={RecipientAddress}></textarea>
   <label htmlFor="comment"> Address</label>
-  {/* <input type="text" onChange={(e) => setRecipientAddress(e.target.value.toUpperCase())} 
-          value={RecipientAddress} /> */}
+  
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setRecipientCity(e.target.value.toUpperCase())} 
           value={RecipientCity}></textarea>
   <label htmlFor="comment"> City</label>
-  {/* <input type="text" onChange={(e) => setRecipientCity(e.target.value.toUpperCase())} 
-          value={RecipientCity}/> */}
+ 
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setRecipientState(e.target.value.toUpperCase())} 
           value={RecipientState}></textarea>
   <label htmlFor="comment">State</label>
-  {/* <input type="text" onChange={(e) => setRecipientState(e.target.value.toUpperCase())} 
-          value={RecipientState} /> */}
+ 
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setRecipientZip(e.target.value.toUpperCase())} 
           value={RecipientZip}></textarea>
   <label htmlFor="comment">Zip</label>
-  {/* <input type="text" onChange={(e) => setRecipientZip(e.target.value.toUpperCase())} 
-          value={RecipientZip} /> */}
+ 
 </div>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setRecipientPhone(e.target.value.toUpperCase())} 
           value={RecipientPhone}></textarea>
   <label htmlFor="comment">Phone</label>
-  {/* <input type="text" onChange={(e) => setRecipientPhone(e.target.value.toUpperCase())} 
-          value={RecipientPhone} /> */}
+ 
 </div>
 <h1>Product Info</h1>
 <div className="form-floating">
   <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setProduct(e.target.value.toUpperCase())} 
           value={Product}></textarea>
   <label htmlFor="comment"> Product</label>
-  {/* <input type="text" onChange={(e) => setProduct(e.target.value.toUpperCase())} 
-          value={Product}/> */}
-</div>
-{/* <div className="form-floating">
-  <textarea className="form-control" id="comment" name="text" placeholder="Comment goes here"onChange={(e) => setDeliveryDate(e.target.value.toUpperCase())} 
-          value={DeliveryDate}></textarea>
-  <label htmlFor="comment"> Delivery Date</label>
   
-</div> */}
+</div>
+
 <div>
 <Calendar onChange={setDeliveryDate} value={DeliveryDate} /></div>
-<button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white'>
+<Button className='border border-blue-500 bg-blue-600 hover:bg-blue-500 w-half p-4 my-2 text-white'>
           Submit
-        </button>
+        </Button>
 </div>
 </form>
    
