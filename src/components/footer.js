4af2c9ac208cb5a072e6a1ccb6778c174bc43b2c
toledosuperman <49626128+toledosuperman@users.noreply.github.com>
2022-11-29 10:21:36 -0500

    edit footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,7 @@ class Footer extends Component {
             <div className="container-fluid px-4">
                 <div className="d-flex align-items-center justify-content-between small">
                     <div className="text-muted">Copyright &copy; FlowerPOS 2022</div>
-                    <div>
-                        <a href="#">Privacy Policy</a>
-                        &middot;
-                        <a href="#">Terms &amp; Conditions</a>
-                    </div>
+                    
                 </div>
             </div>
         </footer>
