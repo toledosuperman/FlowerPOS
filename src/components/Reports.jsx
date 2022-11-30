3a35d1f72da857b5a0dacbd18627077f0e64984a
sayemk23 <go2218@wayne.edu>
2022-11-29 22:06:50 -0500

    added datepicker calendar to report page

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -10,23 +10,36 @@ import background from '../assets/FlowerField.jpg'
 import Footer from './footer';
 import "./account.css";
 import { collection, getDocs, orderBy, limit, query, where} from "firebase/firestore";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { Calendar } from 'react-date-range';
+import DatePicker from './DateRangePicker';
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { addDays } from 'date-fns';
 
 
 function Reports ()  {
   const [isLoading] = useState(false);
   const [user, loading] = useAuthState(auth);
   const [orders, setOrders] = useState([])
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: 'selection'
+    }
+  ]);
+  console.log(state)
+
+
 
   useEffect(() => {
     if (loading) return;
   }, [user, loading]);
-  useEffect(()=>{
-  getOrders()
-},[])
-  useEffect(()=> {
-      console.log(orders)
-  },[orders]
-  )
+  
+
 
   function getOrders(){
     const orderCollectionRef = collection(db,'Orders')
@@ -42,6 +55,25 @@ function Reports ()  {
         .catch(error => console.log(error.message))
         };  
 
+        function getOrdersbyDate(dates){
+          const {startDate, endDate} = dates;
+          console.log(startDate, endDate)
+          if (startDate === undefined && endDate === undefined) {
+            getOrders()
+            return
+          }
+          const orderCollectionRef = collection(db,'Orders')
+          const q = query(orderCollectionRef, where("created", ">=", startDate), where("created", "<=", endDate));
+          getDocs(q)
+              .then(response =>{
+                  const ord = response.docs.map(doc => ({
+                      data: doc.data(),
+                      id: doc.id,
+                  }))
+                  setOrders(ord)
+              })
+              .catch(error => console.log(error.message))
+              };  
 return (<>
   {(user === null) && <NoLoggedInView />}
   {(isLoading === true) && <Spinner animation="border" variant="secondary" />}
@@ -56,6 +88,16 @@ return (<>
                       <div className="align-Products-center" style={{ marginRight: 8 }}>
                           
                           <h4 style={{ marginTop: 8, }}>Reports</h4>
+                        
+
+                          <DateRangePicker
+  onChange={item => setState([item.selection])}
+  showSelectionPreview={true}
+  moveRangeOnFirstSelection={false}
+  months={2}
+  ranges={state}
+  direction="horizontal"
+/>;
                           <DropdownButton id="dropdown-basic-button" title="Select Report">
       <Dropdown.Item href="#/action-1">COGS Report</Dropdown.Item>
       <Dropdown.Item href="#/action-2">Products Report</Dropdown.Item>
