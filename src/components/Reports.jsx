ccb3c0c2c14234852fa3d1d87c506788847d044d
sayemk23 <go2218@wayne.edu>
2022-11-29 20:06:47 -0500

    business report edits

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,18 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { auth} from "../firebase.js";
+import { auth, db} from "../firebase.js";
 import Navbar from './navbar';
 import NoLoggedInView from './NoLoggedInView';
-import { Spinner, Card } from 'react-bootstrap';
+import { Spinner, Card, Dropdown, DropdownButton } from 'react-bootstrap';
 import  { Toaster } from 'react-hot-toast';
 import background from '../assets/FlowerField.jpg'
 import Footer from './footer';
 import "./account.css";
+import { collection, getDocs, orderBy, limit, query, where} from "firebase/firestore";
+
 
 function Reports ()  {
   const [isLoading] = useState(false);
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    if (loading) return;
+  }, [user, loading]);
+  useEffect(()=>{
+  getOrders()
+},[])
+  useEffect(()=> {
+      console.log(orders)
+  },[orders]
+  )
+
+  function getOrders(){
+    const orderCollectionRef = collection(db,'Orders')
+    const q = query(orderCollectionRef, orderBy('created', 'desc'), limit(10))
+    getDocs(q)
+        .then(response =>{
+            const ord = response.docs.map(doc => ({
+                data: doc.data(),
+                id: doc.id,
+            }))
+            setOrders(ord)
+        })
+        .catch(error => console.log(error.message))
+        };  
 
 return (<>
   {(user === null) && <NoLoggedInView />}
@@ -27,9 +55,43 @@ return (<>
                   <Card.Header className="d-flex justify-content-between align-Products-center">
                       <div className="align-Products-center" style={{ marginRight: 8 }}>
                           
-                          <h4 style={{ marginTop: 8, }}>Hello There</h4>
+                          <h4 style={{ marginTop: 8, }}>Reports</h4>
+                          <DropdownButton id="dropdown-basic-button" title="Select Report">
+      <Dropdown.Item href="#/action-1">COGS Report</Dropdown.Item>
+      <Dropdown.Item href="#/action-2">Products Report</Dropdown.Item>
+      <Dropdown.Item href="#/action-3">Profits and Loss</Dropdown.Item>
+    </DropdownButton>
                       </div></Card.Header><Card.Body>
-           
+                      <div className="row">
+                <div className="table-responsive " >
+                 <table className="table table-striped table-hover table-bordered overflow-x:scroll ">
+                 <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Price ($)</th>
+                            <th>Cost ($)</th>
+                            <th>Date Created</th>
+                          
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                 {orders.map((order) => (
+                    <tr key={order.id}>
+                            <td>{order.data.Product}</td>
+                            <td>{order.data.Price}</td>
+                            <td>{order.data.Cost}</td>
+                            <td>{order.data.created.toDate().toDateString()}</td>
+                            
+                        </tr>
+
+                    ))}
+                    </tbody>
+                    </table>
+                        
+                    
+            </div>
+        </div>
         </Card.Body></Card>
         <Toaster toastOptions={{
     success: {
@@ -49,6 +111,7 @@ return (<>
 </React.Fragment></>}
 ;</>)
   ;}
+
 
 
 export default Reports;
