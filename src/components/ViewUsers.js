4593d340157f8dc515790f6ca3c03425560ba268
toledosuperman <49626128+toledosuperman@users.noreply.github.com>
2022-12-01 12:01:30 -0500

    user edit

--- src/components/ViewUsers.js
+++ src/components/ViewUsers.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState , useCallback} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Table, Card, Button, Modal, Form, FloatingLabel, Spinner, InputGroup} from 'react-bootstrap';
+import { Table, Card, Button, Modal, Form, Spinner, InputGroup} from 'react-bootstrap';
 import Navbar from './navbar';
 import Footer from './footer';
 import { UserAuth } from '../context/AuthContext';
@@ -48,49 +48,17 @@ function ViewUsers() {
       }
   }, [user, fetchUsers])
 //more use state react components
-  const [showAddEditForm, setShowAddEditForm] = useState(false);
-  const [addEditFormType, setAddEditFormType] = useState('Edit'); //Add, Edit
-  const [validated, setValidated] = useState(false);
   const [showDeleteDialogue, setShowDeleteDialogue] = useState(false);
   //function to handle closing modals
   const handleModalClose = () => {
-      setShowAddEditForm(false);
+      
       setShowDeleteDialogue(false);
       setCurrentUsersId("");
-      setAddEditFormType();
-      setCurrentUsers({ posusername: " ", role: false, email: " ", uid: "", authProvider: ""})
+     
+      setCurrentUsers({role: false})
       setIsLoading(false);
   }
-  //function to submit update firestore data
-  const handleAddEditFormSubmit = (e) => {
-    e.preventDefault();
-    const { posusername, role, email, uid, authProvider } = e.target.elements;
 
-    // if (posusername.value && uid.value) {
-        // if (addEditFormType === "Add") {
-        //     setIsLoading(true);
-        //     return FirestoreService.AddNewUsers(posusername.value, role.value, uid.value, email.value, authProvider.value).then(() => {
-        //         toast.success(`${posusername.value} is successfully added.`)
-        //         handleModalClose();
-        //         window.location.reload(false);
-        //     }).catch((e) => {
-        //         toast.error("Error occured: " + e.message);
-        //         setIsLoading(false);
-        //     })
-         if (addEditFormType === "Edit") {
-            setIsLoading(true);
-            return FirestoreService.UpdateUsers(currentUsersId, posusername.value, role.value, uid.value, email.value,authProvider.value).then(() => {
-                toast.success(`${posusername.value} is successfully updated.`);
-                handleModalClose();
-                window.location.reload(false);
-            }).catch((e) => {
-                toast.error("Error occured: " + e.message);
-                setIsLoading(false);
-            })
-        }
-        setValidated(true)
-    }
-    
 
 //function to delete firestore data
   const handleUsersDelete = async (e) => {
@@ -120,33 +88,7 @@ function ViewUsers() {
    }}>
    <Navbar />
   
-   <Modal show={showAddEditForm} onHide={handleModalClose}>
-                  <Form noValidate validated={validated} onSubmit={handleAddEditFormSubmit}>
-                      <Modal.Header closeButton>
-                          <Modal.Title>{(addEditFormType === 'Add') ? 'Add User' : 'Edit'}</Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                          <FloatingLabel controlId="posusername" label="User Name" className="mb-3" >
-                              <Form.Control required type='text' placeholder='Enter User Name' size='md' value={setUsers.posusername} onChange={(e) => {
-                                  setCurrentUsers({
-                                      "posusername": e.target.value,
-                                      
-                                      
-                                      
-                                  })
-                              }} />
-                              <Form.Control.Feedback type='invalid'>User Name is required</Form.Control.Feedback>
-                          </FloatingLabel>
-
-                          
-
-                         
-                      </Modal.Body>
-                      <Modal.Footer>
-                          <Button type="submit">{(addEditFormType === 'Add') ? 'Add' : 'Update'}</Button>
-                      </Modal.Footer>
-                  </Form>
-              </Modal>
+ 
           
 
               {/* Delete Confirmation Dialogue START */}
@@ -204,19 +146,16 @@ function ViewUsers() {
                                           <Button variant='primary' onClick={() => {
                                               setCurrentUsersId(user.doc.key.path.segments[user.doc.key.path.segments.length - 1])
                                               setCurrentUsers({
-                                                  "name": user.doc.data.value.mapValue.fields.posusername.stringValue,
-                                                  "email": user.doc.data.value.mapValue.fields.email.stringValue,
-                                                  "role": user.doc.data.value.mapValue.fields.role.booleanValue
+                                                  
+                                                  "role": !user.doc.data.value.mapValue.fields.role.booleanValue
                                                   
                                               });
-                                              setAddEditFormType();
-                                              setShowAddEditForm(true);
-                                          }}>✎ Edit</Button>{' '}
+                                              
+                                          }}>Change Permissions</Button>{' '}
                                           <Button variant='danger' onClick={() => {
                                               setCurrentUsersId(user.doc.key.path.segments[user.doc.key.path.segments.length - 1]);
                                               setCurrentUsers({
-                                                  "name": user.doc.data.value.mapValue.fields.posusername.stringValue,
-                                                  "email": user.doc.data.value.mapValue.fields.email.stringValue ,
+                                                  
                                                   "role": user.doc.data.value.mapValue.fields.role.booleanValue,
                                                   
                                                 });
